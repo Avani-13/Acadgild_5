16eb34fe91ae08e5bbedce5607980b9ad5a8bbfa
Avani-13 <36336778+Avani-13@users.noreply.github.com>
2018-05-01 22:20:59 +0530

    Add files via upload

---
+++ Task2/Mapper2.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import java.util.*;
+
+
+public class Mapper2
+  extends Mapper<LongWritable, Text, IntWritable, IntWritable> 
+{
+	 private final static IntWritable one = new IntWritable(1);	
+	 private final static IntWritable zero = new IntWritable();
+	 IntWritable t1;
+	 IntWritable t2;
+	 public void setup(Context context)
+	 {
+		 t1 = new IntWritable();
+		 t2 = new IntWritable();
+	 }
+	public void map(LongWritable key, Text value, Context context)
+      throws IOException, InterruptedException 
+{   
+	String[] lineArray = value.toString().split("\\|");
+	if(Integer.parseInt(lineArray[4])==1)
+	{
+		t1.set(Integer.parseInt(lineArray[1]));
+	    context.write(t1, one); 
+	}
+		else
+		{
+			t2.set(Integer.parseInt(lineArray[1]));
+		    context.write(t2, zero); 
+		}
+	}
+}
+	
---
+++ Task3/Reducer3.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+//import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+
+public class Reducer3
+  extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+  
+  @Override
+  
+  public void reduce(IntWritable key, Iterable<IntWritable> values,
+      Context context)
+      throws IOException, InterruptedException {
+      System.out.println("TrackId=>"+key) ;	
+    
+      int sum = 0;
+      for (IntWritable value : values) {
+    	  sum+=value.get();	
+       }		
+       context.write(key, new IntWritable(sum));
+  }
+}
